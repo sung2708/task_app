43988cfd41222cbad6925a89ed1ed73dd779b705
sung2708 <aki18082002@gmail.com>
2025-05-09 15:02:08 +0700

    Refactor: Delele view

--- app.js
+++ app.js
@@ -1,31 +1,29 @@
-import express from 'express';
-import dotenv from'dotenv';
-import connectDB from './config/db.js';
-import taskRoute from './routes/task.route.js';
-import userRoute from './routes/user.route.js';
-import groupRoue from './routes/group.route.js';
-import cors from 'cors';
+import express from "express";
+import dotenv from "dotenv";
+import connectDB from "./config/db.js";
+import taskRoute from "./routes/task.route.js";
+import userRoute from "./routes/user.route.js";
+import groupRoue from "./routes/group.route.js";
+import cors from "cors";
 
 dotenv.config();
 
 var app = express();
 connectDB();
 
-app.set('view engine', 'ejs');
-app.use('/static', express.static('public'))
 app.use(express.json());
 app.use(cors());
 
-app.get('/',(req, res)=>{
-    res.render('home');
+app.get("/", (req, res) => {
+    res.render("home");
 });
 
-app.use('/api/tasks', taskRoute);
-app.use('/api/user', userRoute);
-app.use('/api/group', groupRoue);
+app.use("/api/tasks", taskRoute);
+app.use("/api/user", userRoute);
+app.use("/api/group", groupRoue);
 
 var port = process.env.PORT || 3001;
 
-app.listen(port, () => 
-    console.log(`Server running on http://localhost:${port}`)
+app.listen(port, () =>
+    console.log(`Server running on http://localhost:${port}`),
 );
